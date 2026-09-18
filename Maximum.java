79aa038564dfeac259e6c00839daf28c74f13021
anand-emc <manand8090@gmail.com>
2026-09-18 12:09:48 +0530

    Add files via upload

---
+++ Maximum.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+class Maximum {
+
+    // Generic function to find maximum
+    public static <T extends Comparable<T>> T findMax(T[] arr) {
+        T max = arr[0];
+
+        for (T element : arr) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        System.out.println("Choose data type:");
+        System.out.println("1. Integer");
+        System.out.println("2. Double");
+        System.out.println("3. Character");
+        System.out.println("4. String");
+
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+
+            case 1:
+                Integer[] a = new Integer[n];
+
+                System.out.println("Enter elements:");
+                for (int i = 0; i < n; i++)
+                    a[i] = sc.nextInt();
+
+                System.out.println("Maximum Element = " + findMax(a));
+                break;
+
+            case 2:
+                Double[] b = new Double[n];
+
+                System.out.println("Enter elements:");
+                for (int i = 0; i < n; i++)
+                    b[i] = sc.nextDouble();
+
+                System.out.println("Maximum Element = " + findMax(b));
+                break;
+
+            case 3:
+                Character[] c = new Character[n];
+
+                System.out.println("Enter elements:");
+                for (int i = 0; i < n; i++)
+                    c[i] = sc.next().charAt(0);
+
+                System.out.println("Maximum Element = " + findMax(c));
+                break;
+
+            case 4:
+                String[] d = new String[n];
+
+                System.out.println("Enter elements:");
+                for (int i = 0; i < n; i++)
+                    d[i] = sc.next();
+
+                System.out.println("Maximum Element = " + findMax(d));
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        sc.close();
+    }
+}
